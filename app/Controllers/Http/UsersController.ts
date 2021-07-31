9ec8f240d52bbf8175dcbe27af8a0c041488fd7e
Maycon Santos <maycon.s.santos44@gmail.com>
2021-07-31 13:38:58 -0300

    Update Documentation

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,23 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class UsersController {
+  public async index({}: HttpContextContract) {
+    return ''
+  }
+
+  public async store({}: HttpContextContract) {
+    return ''
+  }
+
+  public async show({}: HttpContextContract) {
+    return ''
+  }
+
+  public async update({}: HttpContextContract) {
+    return ''
+  }
+
+  public async destroy({}: HttpContextContract) {
+    return ''
+  }
+}
